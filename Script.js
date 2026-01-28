96162be577d8a3d835da821023a85cb5f5fc6890
Sagarmore1122 <126661572+Sagarmore1122@users.noreply.github.com>
2026-01-28 10:23:46 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,3 +4,45 @@
     alert("Form submitted successfully!");
   });
 
+
+function scrollToForm() {
+  const form = document.getElementById("appointment-form");
+  if (form) {
+    form.scrollIntoView({
+      behavior: "smooth",
+      block: "center"
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const images = document.querySelectorAll(".simple-carousel img");
+  const nextBtn = document.querySelector(".next-btn");
+  const prevBtn = document.querySelector(".prev-btn");
+
+  let current = 0;
+
+  function showImage(index) {
+    images.forEach(img => img.classList.remove("active"));
+    images[index].classList.add("active");
+  }
+
+  nextBtn.addEventListener("click", () => {
+    current = (current + 1) % images.length;
+    showImage(current);
+  });
+
+  prevBtn.addEventListener("click", () => {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+  });
+
+  showImage(current);
+});
+
+
+
+
+
+
